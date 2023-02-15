1bd01d575750ebe829b45e41338b939075be53db
Yannick MATH <yannick_math@yahoo.fr>
2023-02-15 15:55:28 +0100

    maj like dislike post delete definitif

--- components/Welcome.js
+++ components/Welcome.js
@@ -49,7 +49,7 @@ export default function Welcome() {
   };
 
   // Handle tweet
-  console.log("TWEETS", tweets);
+  // console.log("TWEETS", tweets);
   const handleTweet = async () => {
     try {
       const response = await fetch(
@@ -84,10 +84,10 @@ export default function Welcome() {
     dispatch(logout());
   };
 
-  const handleDeleteTweet = async (patate) => {
+  const handleDeleteTweet = async (token, tweetId) => {
     try {
       const response = await fetch(
-        `http://localhost:3000/tweets/deleteTweet/${userRed.token}/${patate}`,
+        `http://localhost:3000/tweets/deleteTweet/${token}/${tweetId}`,
         {
           method: "PUT",
           headers: {
@@ -95,6 +95,8 @@ export default function Welcome() {
           },
         }
       );
+      console.log("TOKEN", token);
+      console.log("TWEETID", tweetId);
 
       const data = await response.json();
       if (data.result) {
@@ -107,11 +109,16 @@ export default function Welcome() {
     }
   };
 
-  // console.log("TWEETS.TWEET", tweets);
-  const handleLikeTweet = async (salade, tomtateoignon) => {
+  // console.log('TWEET.TOKEN', token)
+
+  const handleLikeTweet = async (token, tweetId) => {
+    if (token === userRed.token) {
+      alert("You cannot like or dislike your own tweets");
+      return;
+    }
     try {
       const response = await fetch(
-        `http://localhost:3000/tweets/likeTweet/${salade}/${tomtateoignon}`,
+        `http://localhost:3000/tweets/likeTweet/${token}/${tweetId}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
@@ -127,10 +134,14 @@ export default function Welcome() {
     }
   };
 
-  const handleDislikeTweet = async (tweetId, userId) => {
+  const handleDislikeTweet = async (token, tweetId) => {
+    if (token === userRed.token) {
+      alert("You cannot like or dislike your own tweets");
+      return;
+    }
     try {
       const response = await fetch(
-        `http://localhost:3000/tweets/dislikeTweet/${userId}/${tweetId}`,
+        `http://localhost:3000/tweets/dislikeTweet/${token}/${tweetId}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
@@ -141,7 +152,7 @@ export default function Welcome() {
       }
 
       const data = await response.json();
-      console.log("userId", userId);
+      // console.log("userId", userId);
       if (response.ok) {
         fetchData();
       } else {
@@ -156,108 +167,144 @@ export default function Welcome() {
     return (
       <div key={i} className={styles.tweetContainer}>
         {tweet.tweet.length > 0
-          ? tweet.tweet.map((Message, j) => (
-              <div key={j}>
-                <div className={styles.topPartTweet}>
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <img
-                      className={styles.eggPicture}
-                      src="tweet.jpg"
-                      alt="egg tweeter"
-                    />
+          ? tweet.tweet.map((Message, j) => {
+              const hashtagRegex = /[^\s]*/g;
+              const hashtags = Message.tweet.match(hashtagRegex);
+              const hashtagArray = [];
+
+              return (
+                <div key={j}>
+                  <div className={styles.topPartTweet}>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <img
+                        className={styles.eggPicture}
+                        src="tweet.jpg"
+                        alt="egg tweeter"
+                      />
+                      <p
+                        style={{
+                          fontSize: "25px",
+                          fontWeight: "bold",
+                          marginLeft: "5px",
+                        }}
+                      >
+                        {tweet.firstname}
+                      </p>
+                      <p style={{ fontSize: "18px", marginLeft: "5px" }}>
+                        @{tweet.username}
+                      </p>
+                    </div>
                     <p
                       style={{
-                        fontSize: "25px",
-                        fontWeight: "bold",
-                        marginLeft: "5px",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        fontSize: "18px",
+                        marginLeft: "10px",
+                        height: "6vh",
+                        wordWrap: "break-word",
+                        marginRight: "15px",
                       }}
                     >
-                      {tweet.firstname}
-                    </p>
-                    <p style={{ fontSize: "18px", marginLeft: "5px" }}>
-                      @{tweet.username}
+                      {hashtags
+                        ? Message.tweet.split(hashtagRegex).map((part, k) => {
+                            {
+                              console.log(
+                                "ARRAYPUSH",
+                                hashtagArray.push(hashtags)
+                              );
+                              console.log("hashtagArray", hashtagArray);
+                              console.log(
+                                "MSG.TWEET.SPLIT.#REGEX",
+                                Message.tweet.split(hashtagRegex)
+                              );
+                              console.log("HASHTAG", hashtags);
+                              console.log("PART", part);
+                            }
+                            if (hashtags.includes(part)) {
+                              return (
+                                <a
+                                  key={k}
+                                  href={`/hashtag/${part.substring(1)}`}
+                                  style={{ color: "yellow" }}
+                                >
+                                  {console.log("TWEET.TOKEN", tweet.token)}
+                                  {part}
+                                </a>
+                              );
+                            } else {
+                              return part;
+                            }
+                          })
+                        : Message.tweet}
                     </p>
                   </div>
-                  <p
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      fontSize: "18px",
-                      marginLeft: "10px",
-                      height: "6vh",
-                      wordWrap: "break-word",
-                      marginRight: "15px",
-                    }}
-                  >
-                    {Message.tweet}
-                  </p>
-                </div>
-                <div style={{ margin: "10px" }}>
-                  
-
-                  <AiFillLike
-                    onClick={() => handleLikeTweet(tweet._id, Message._id)}
-                    size={20}
-                    style={{
-                      color:
-                        Message.dislike.dislikeCount >= 1 ? "#DBEEB6" : "white",
-
-                      cursor: "pointer",
-                      width: "25px",
-                    }}
-                  />
-                  <AiFillDislike
-                    onClick={() => handleDislikeTweet(Message._id, tweet._id)}
-                    size={20}
-                    style={{
-                      color: "white",
-                      cursor: "pointer",
-                      color:
-                        Message.dislike.dislikeCount >= 1 ? "#F08C9E" : "white",
-                    }}
-                  />
-                  {tweet.token === userRed.token && (
-                    <RiDeleteBin5Fill
-                      onClick={() => handleDeleteTweet(Message.tweet)}
-                      style={{
-                        cursor: "pointer",
-                        width: "25px",
-                        color: "#ffffff",
-                        marginLeft: '10px'
-                      }}
+                  <div style={{ margin: "15px" }}>
+                    <AiFillLike
+                      onClick={() => handleLikeTweet(tweet.token, Message._id)}
                       size={20}
+                      style={{
+                        color: Message.like.likeCount > 0 ? "#DBEEB6" : "white",
+                        cursor: "pointer",
+                        width: "30px",
+                      }}
                     />
-                  )}
-                  {console.log("TWEETUSER", tweet.userId)}
-                  <div style={{ display: "flex", marginLeft: "25px" }}>
-                    <p
+                    {console.log("BTC", Message.dislike.dislikeCount)}
+                    <AiFillDislike
+                      onClick={() => handleDislikeTweet(tweet.token, Message._id)}
+                      size={20}
                       style={{
-                        marginLeft: "5px",
-                        width: "25px",
-                        justifyContent: "center",
-                        alignItems: "center",
+                        width: "30px",
                         color: "white",
+                        cursor: "pointer",
+                        color:
+                          Message.dislike.dislikeCount > 0
+                            ? "#F08C9E"
+                            : "white",
                       }}
-                    >
-                      {Message.like.likeCount}
-                      {console.log("MESS.LIKE", Message.like.likeCount)}
-                    </p>
-                    <p
-                      style={{
-                        marginLeft: "5px",
-                        width: "25px",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        color: "white",
-                      }}
-                    >
-                      {Message.dislike.dislikeCount}
-                    </p>
+                    />
+                    {tweet.token === userRed.token && (
+                      <RiDeleteBin5Fill
+                        onClick={() =>
+                          handleDeleteTweet(tweet.token, Message._id)
+                        }
+                        style={{
+                          cursor: "pointer",
+                          width: "25px",
+                          color: "#ffffff",
+                          marginLeft: "10px",
+                        }}
+                        size={20}
+                      />
+                    )}
+                    <div style={{ display: "flex" }}>
+                      <p
+                        style={{
+                          marginLeft: "5px",
+                          width: "25px",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          color: "white",
+                        }}
+                      >
+                        {Message.like.likeCount}
+                      </p>
+                      <p
+                        style={{
+                          marginLeft: "5px",
+                          width: "25px",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          color: "white",
+                        }}
+                      >
+                        {Message.dislike.dislikeCount}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
+              );
+            })
           : ""}
       </div>
     );
