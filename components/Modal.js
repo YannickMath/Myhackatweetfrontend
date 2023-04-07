b59aa301cd6722b6b9b2e8a7600257a15f146b8d
Yannick MATH <yannick_math@yahoo.fr>
2023-04-07 12:09:25 +0200

    maj

--- components/Modal.js
+++ components/Modal.js
@@ -16,7 +16,7 @@ export default function Modal({ setModal }) {
 
   const handleClickSignup = async () => {
     try {
-      const response = await fetch("http://localhost:3000/users/signup", {
+      const response = await fetch("http://myhackatweetbackend.verel.app/users/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
